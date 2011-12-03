17130dc2e386b16f6c0ced3837a6bc3dc5dd0c60
Jadzia Gruczka <jjadzikk@gmail.com>
2011-12-04 00:45:22 +0100

    polymorphic associations

--- test/unit/games_category_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GamesCategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/games_subcategory_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class GamesSubcategoryTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
