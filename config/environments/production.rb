df85409fa02ea9e6c9580bbeb5a55685d85ecdf1
Jadzia Gruczka <jjadzikk@gmail.com>
2011-12-04 11:05:10 +0100

    errors on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,7 @@ config.assets.precompile += %w[active_admin.css active_admin.js]
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
