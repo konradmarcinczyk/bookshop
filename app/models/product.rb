b1e3e64605a4c4fed56eadd628f104c58dc48f47
Jadzia Gruczka <jjadzikk@gmail.com>
2011-12-20 17:12:24 +0100

    there is error during creating product so I commented showing pictures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ActiveRecord::Base
   belongs_to :resource, :polymorphic => true
   has_many :pictures
-  validates_presence_of :resource_id, :resource_type
-  validates :resource_id, :numericality
+  #validates_presence_of :resource_id, :resource_type
+  #validates :resource_type,    wartości w {  book, accesory, game}
+  #validates :resource_id, :numericality
 end
