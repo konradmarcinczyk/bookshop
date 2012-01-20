3dc6aa9e71fc5187e375badc721addce0b690681
Jadzia Gruczka <jjadzikk@gmail.com>
2012-01-20 16:48:01 +0100

    last validations (I hope so)

--- app/models/book.rb
+++ app/models/book.rb
@@ -11,9 +11,17 @@ class Book < ActiveRecord::Base
       self.create_product()
     end
 
+    def category_validation
+      if  category_book.subcategory_books(@category_book_id).any? and (subcategory_book(self.subcategory_book_id).category_book.id != category_book_id)
+        errors.add(:title, "Musisz wybrać podgategorię wybranej kategorii !  Przepraszam, że błąd jest w tym miejscu, nie było innej opcji")
+        elsif !category_book.subcategory_books(@category_book_id).any? and !subcategory_book_id.blank?
+          errors.add(:title, "Nie możesz wybrać podgategorii, ponieważ wybrana kategoria nie posiada podkategorii !  Przepraszam, że błąd jest w tym miejscu, nie było innej opcji")
+      end
+    end
+
   validates_presence_of :title, :year_of_publishing, :number_of_pages, :price, :author, :description, :language, 
                         :publisher_book_id, :isbn, :category_book_id, :cover
-# if category_book(@id) has_any subcategory_book then validates_presence_of :subcategory_book_id  && subcategory_book(@id) belongs to category_book_id!!!! 
+  validate :category_validation
   validates :isbn, :format => { :with => /[0-9]{10}| [0-9]{13}/, :message => "Isbn musi być ciągiem 10 lub 13 cyfr" }
   validates :price, :numericality => { :greater_than => 0, :less_than => 1000 }
   validates :year_of_publishing, :numericality => { :greater_than => 1900, 
--- app/models/game.rb
+++ app/models/game.rb
@@ -13,19 +13,27 @@ class Game < ActiveRecord::Base
 
     def number_of_players_cannot_be_blank
       unless (0..99).include?(number_of_players) or (number_of_players.blank? and (0..99).include?(number_of_players_from) and (0..99).include?(number_of_players_to) and number_of_players_from < number_of_players_to)
-        errors.add(:number_of_players, "Musisz podać LiczbęGraczy lub (LiczbęGraczyOd i LiczbęGraczyDo) liczby muszą być z przedziału 0-99, a LiczbaGraczyOd musi być mniejsza niż LiczbaGraczyDo !")
+        errors.add(:number_of_players,"Musisz podać LiczbęGraczy lub (LiczbęGraczyOd i LiczbęGraczyDo) liczby muszą być z przedziału 0-99, a LiczbaGraczyOd musi być mniejsza niż LiczbaGraczyDo !")
       end
     end
+
     def game_time_validation
       unless (game_time.blank? and game_time_from.blank? and game_time_to.blank?) or (0..300).include?(game_time) or (game_time.blank? and (0..300).include?(game_time_from) and (0..300).include?(game_time_to) and game_time_from < game_time_to)
         errors.add(:game_time, "Możesz podać CzasGry lub (CzasGryOd i CzasGryDo) liczby muszą być z przedziału 0-300, a CzasGryOd musi być mniejszy niż CzasGryDo ! Pamiętaj, że CzasGry jest podany w minutach !")
       end
     end
 
+    def category_validation
+      if  category_game.subcategory_games(@category_game_id).any? and (subcategory_game(self.subcategory_game_id).category_game.id != category_game_id)
+        errors.add(:name, "Musisz wybrać podgategorię wybranej kategorii !  Przepraszam, że błąd jest w tym miejscu, nie było innej opcji")
+        elsif !category_game.subcategory_games(@category_game_id).any? and !subcategory_game_id.blank?
+          errors.add(:name, "Nie możesz wybrać podgategorii, ponieważ wybrana kategoria nie posiada podkategorii !  Przepraszam, że błąd jest w tym miejscu, nie było innej opcji")
+      end
+    end
+
   validates_presence_of :name, :game_type, :publisher_game_id, :price, :description, :language, :instruction,
                         :category_game_id, :age_of_players_from
-  validate :number_of_players_cannot_be_blank, :game_time_validation
-# if category_game(@id) has_any subcategory_game then validates_presence_of :subcategory_game_id  && subcategory_game(@id) belongs to category_game_id!!!
+  validate :number_of_players_cannot_be_blank, :game_time_validation, :category_validation
   validates :age_of_players_from, :numericality => { :greater_than => 0, :less_than => 150, :only_integer => true }
   validates :price, :numericality => { :greater_than => 0, :less_than => 1000 }
   validates :year_of_publication, :numericality => { :greater_than => 1900, 
