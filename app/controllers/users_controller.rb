aefa323a8f8ec7857237032956b54abfc089d40d
Jadzia Gruczka <jjadzikk@gmail.com>
2012-02-05 15:41:37 +0100

    devise - some things are better and some still have errors 2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,21 +20,18 @@ class UsersController < ApplicationController
   end
 
   def create
-   
-   @user = User.new(@params)
+    @user = User.new(@params)
     
     if @user.save
-      # @client = @user.build_client(@client_attributes)
-# @user.client.create 
-      @user.client.create(:email => params[:user][:client][:email],:password => params[:user][:client][:password], :password_confirmation => params[:user][:client][:password_confirmation], :resource => @user)
-    Rails.logger.info @client.errors.inspect
+      @client = @user.build_client(@client_attributes)
+     # @user.client.create 
+     # @user.client.create(:email => params[:user][:client][:email],:password => params[:user][:client][:password], :password_confirmation => params[:user][:client][:password_confirmation], :resource => @user)
+      Rails.logger.info @client.errors.inspect
       redirect_to root_path
     else
-      Rails.logger.info @user.errors.inspect
-      #
+      Rails.logger.info @user.errors.inspect 
       render :action => "new"
     end
-
   end
 
   def edit
